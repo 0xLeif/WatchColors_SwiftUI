47ccf2db898c02428de01c46daec096b30d16fa6
zmeriksen <zmeriksen@gmail.com>
2019-07-12 14:11:22 -0500

    Initial Commit

---
+++ WatchColors WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WatchColors WatchKit Extension
+//
+//  Created by Zach Eriksen on 7/12/19.
+//  Copyright © 2019 oneleif. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController : WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
